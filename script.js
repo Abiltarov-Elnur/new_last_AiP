40b874c5a6cca94f7d70a0886c4a07589b8af6c3
Elnur Abiltarov <Asus@DESKTOP-RV00K7V>
2024-11-02 10:52:38 +0300

    Задание 45

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 "use strict";
-let a = 1, b = 2, c =3, t = true;
-
-console.log(a);
-console.log(b, c);
-console.log('123');
-
-console.log(t);
-
-console.log(eee);
-
 let PI = 3.14, r = 3;
 
 console.log(PI * (r**2));
+
+
+let a = '10', b = '20';
+
+console.log(Number(a) + Number(b));
+
+
+let q = +'10', w = +'20';
+console.log(q+w);
