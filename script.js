3529759ffb4ef1efa1d120fc7390504ac53d16f7
Elnur Abiltarov <Asus@DESKTOP-RV00K7V>
2024-11-05 13:28:38 +0300

    Задание 49

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 "use strict";
-let PI = 3.14, r = 3;
+let n1=parseInt('5px'), n2=parseInt('6px');
+alert(n1+n2);
 
-console.log(PI * (r**2));
-
-
-let a = '10', b = '20';
-
-console.log(Number(a) + Number(b));
+let n3=parseFloat('5.5px'), n4=parseFloat('6.25px');
+let n34 = n3%n4;
+console.log(n34 + 'px');
 
 
-let q = +'10', w = +'20';
-console.log(q+w);
+let n5 = 3, n6 = 6;
+
+alert(String(n5) + String(n6));
+
+
+
+
+let n7 = 1234, n8 = 5678;
+
+let str1 = String(n7).length;
+let str2 = String(n8).length;
+
+alert(str1  +str2);
