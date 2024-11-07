bda5b198a5e013d8adedaa30808f6f2f2f8f7b30
Elnur Abiltarov <Asus@DESKTOP-RV00K7V>
2024-11-07 16:15:38 +0300

    Задание 70

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-let r = prompt('R = circle: '), pi = 3.14;
-s = pi*r**2;
-alert('S sircle: '+ s);
+let arr = [1, 2, 4, 'ar', 'arh', 'bo'];
+
+console.log(arr);
 
 
-let a = prompt('a = square: ');
-s = a**2;
-alert('S square: '+ s);
+
+let arr1 = [1, 2, 3];
+
+alert(arr1[0] + arr1[1] + arr1[2]);
+
+
+
+let arr2 = ['a', 'b', 'c', 'd'];
+
+alert(arr2[0] + '+' + arr2[1] + '+' + arr2[2] + '+' + arr2[3]);
+
+
+
+
+let arr3 = ['a', 'b', 'c', 'd'];
+
+alert(arr3.length);
+alert(arr3[arr3.length-1]);
