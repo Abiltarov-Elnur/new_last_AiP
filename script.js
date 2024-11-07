1593834cf19cbf6db2f91493ee6f5134efd33bfa
Elnur Abiltarov <Asus@DESKTOP-RV00K7V>
2024-11-07 16:01:51 +0300

    Задание 66

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-let str = 'text';
-document.write('<i>' + str + '</i>'); 
+let sec = 60, min = 60, hour = 24;
+
+let sutki = 60*60*24;
+
+alert('sutki ' + sutki);
+
+
+
+alert('30 sutki ' + 30 * sutki);
+
+
+alert('year ' + 365 * sutki);
+
+
+alert('min v sutkah ' + sutki / 60);
