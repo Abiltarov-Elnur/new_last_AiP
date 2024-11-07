6e9a247a1769f1f7beb2daea6e3d154640605b72
Elnur Abiltarov <Asus@DESKTOP-RV00K7V>
2024-11-07 16:45:56 +0300

    Задание 73

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
-let arr = [1, 2, 4, 'ar', 'arh', 'bo'];
+let arr = ['a', 'b', 'c'];
 
-console.log(arr);
+arr[0] = '1';
+arr[1] = '2';
+arr[2] = '3';
 
+alert(arr);
+
+arr[0] += '3';
+arr[1] += '3';
+arr[2] += '3';
+
+alert(arr);
 
 
 let arr1 = [1, 2, 3];
 
-alert(arr1[0] + arr1[1] + arr1[2]);
+arr1[0] += 1;
+arr1[1] += 2;
+arr1[2] += 3;
 
-
-
-let arr2 = ['a', 'b', 'c', 'd'];
-
-alert(arr2[0] + '+' + arr2[1] + '+' + arr2[2] + '+' + arr2[3]);
-
-
-
-
-let arr3 = ['a', 'b', 'c', 'd'];
-
-alert(arr3.length);
-alert(arr3[arr3.length-1]);
+alert(arr1);
