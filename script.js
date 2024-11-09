276aca70f54785ff488d1aa40d1876c8a8e25e42
Elnur Abiltarov <Asus@DESKTOP-RV00K7V>
2024-11-09 14:24:44 +0300

    Заданеи 85

--- script.js
+++ script.js
@@ -1,15 +1,51 @@
-let arr = [1, 2, 3, 4, 5];
-console.log(arr[arr.length-1]);
-
-let arr1 = [1, 2, 3, 4, 5];
-console.log(arr[1] + arr[2] + arr[3] + arr[4] + arr[5]); 
+//Задаени 81
 
 let obj = {1: 'Понедельник', 2: "Вторник", 3: "Среда", 4: "Четверг", 5: "Пятница", 6: "Суббота", 7:"Воскресенье"};
 
-alert(obj[1]);
-alert(obj[2]);
-alert(obj[3]);
-alert(obj[4]);
-alert(obj[5]);
-alert(obj[6]);
-alert(obj[7]);
+console.log(obj);
+
+
+//Задание 82
+
+let obj1 = {a: 1, b: 2, ccc: 'user'};
+
+console.log(obj1['ccc']);
+
+
+//Задание 83
+let date = {year: 2024, month: 11, day: 9 };
+
+console.log(date.year, date.month, date.day);
+//Задание 84
+
+
+let obj2 = {
+	'1a': 1,
+	b2: 2,
+	'a-с': 6,
+	'd 4': 4,
+	e5: 5
+};
+
+console.log(obj2['1a']);
+console.log(obj2.b2);
+console.log(obj2['a-c']);
+console.log(obj2['d 4']);
+console.log(obj2.e5);
+
+//Задание 85
+
+let obj3 = {x: 1, y: 2, z: 3};
+
+obj3.x = obj3.x**2
+//obj3.y = obj3.y**2
+//obj3.z = obj3.z**2
+
+console.log(obj3.x, obj3.y, obj3.z )
+
+
+
+//Задание
+
+
+//Задание
