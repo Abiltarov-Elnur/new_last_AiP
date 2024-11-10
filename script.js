992d2e58e475f8882667e5ea283e21158772e27b
Elnur Abiltarov <Asus@DESKTOP-RV00K7V>
2024-11-10 22:28:54 +0300

    Задание 128

--- script.js
+++ script.js
@@ -1,72 +1,80 @@
-//Задание 109
+//Задание 121
 
-let test = 'abc'
+let test = true;
 
-if(test == 'abc'){
-    console.log('+++');
-} else {
-    console.log('---');
-}
-
-//Задание 110
-
-let test1 = '123';
-let test2 = 123;
-if(test1 == test2){
-    console.log('+++');
-} else {
-    console.log('---');
-}
-
-
-//Задание 113
-
-num = 3;
-if (num > 0 && num < 5){
-    console.log('++=');
-}
-
-num1 = 4;
-
-if ((num <= 1)&&(num1>=3)){
-    console.log('++=');
-}
-
-//Задание 117
-
-if (!(num1 >= 0 || num2 <= 10)) {
+if (!test) {
 	console.log('+++');
 } else {
 	console.log('---');
 }
 
-//Задание 118
+//Задание 123
 
-let test11 = true, test22 = false;
-
-if (test11 === true){
-    console.log('+==');
-}
-if (test22 != true){
-    console.log('+==');
-}
-
-//Задание 119
-
-let t1 = 1;
-
-if(t1 === true){
-    console.log('+=');
-}else{
-    console.log('---');
-}
-
-//Задание 108
-
+let test1 = true;
+let test2 = true;
 let test3 = true;
 
-if (test3) {
+if (test1 || test2 && test3) { 
 	console.log('+++');
 } else {
 	console.log('---');
 }
+
+//Задание 124
+
+let test4 = 10;
+if(test4 == 10){
+    console.log('yes');
+}
+
+//Задание 125
+
+if (test > 0) console.log('+++');
+
+
+//Задание 127
+
+let day = 34;
+
+if(day >= 0 && day <= 10){
+    console.log('Первая декада');
+}else if(day >= 11 && day <= 20){
+    console.log('Вторая декада');
+}else if(day >= 21 && day <= 31){
+    console.log('Третья декада');
+}else{
+    console.log('Ты мне втрираешь какую-то дичь')
+}
+
+//Задание 128
+
+let num = 8;
+
+if(num >=10){
+    
+    if(num <= 99){
+        num = (num%10) + ((num/10)-((num%10)/10));
+        console.log(num)
+    }else{
+        console.log('Дичь')
+    }
+}else{
+    console.log('Дичь')
+}
+
+//Задание 109
+
+
+
+//Задание 109
+
+
+
+//Задание 109
+
+
+
+//Задание 109
+
+
+
