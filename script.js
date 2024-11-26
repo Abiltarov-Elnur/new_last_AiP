9fe9da9ae051bc71e7a2a48aa922a5d1873512a2
Elnur Abiltarov <Asus@DESKTOP-RV00K7V>
2024-11-26 13:56:11 +0300

    Задание 145

--- script.js
+++ script.js
@@ -1,53 +1,50 @@
-//Задание 135
+//Задание 140
 
-let age = 19;
-let res;
+let numm = 10;
+if((numm%2)===0){
+	console.log('Ахрененно');
+}else{
+	console.log('дувак');
+}
 
-if (age >= 18) {
-	res;
-	
-	if (age <= 23) {
-		res = 'от 18 до 23';
-	} else {
-		res = 'больше 23';
-	}
+
+
+//Задание 141
+
+let num1 = '123033'; // берем в кавычки, чтобы обращаться к цифрам 
+
+let sum1 = Number(num1[0]) + Number(num1[1]) + 
+	Number(num1[2]); 
+let sum2 = Number(num1[3]) + Number(num1[4]) + 
+	Number(num1[5]); 
+
+if (sum1 == sum2) {
+	console.log('суммы равны');
 } else {
-	res = 'меньше 18';
+	console.log('суммы не равны');
+}
+//Задание 142
+
+let str = '1234';
+
+if(Number(str[0]) == 1){
+    console.log('++++');
 }
 
-console.log(res);
+//Задание 144
 
-//Задание 137
+let mass = ['a', 'b', 'c', 'd', 'e'];
 
-let arr = [1, 2, 3];
-let res1;
-
-if(arr.length == 3){
-    res1 = arr[0] + arr[2] + arr[1];
-    console.log(res1);
+for(let el of mass){
+	console.log(el);
 }
+//Задание 145
 
-//Задание 138
+let obj = {x: 1, y: 2, z: 3};
 
-let str = 'abcd';
-
-if(str[0] == 'a'){
-    console.log('+++');
+for (let kk in obj){
+	console.log(kk); //вывели название ключей
 }
-
-//Задание 139.1
-
-let num = 12340;
-let str1 = String(num)
-if ((str1[str1.length-1]) == 0){
-    console.log('++');
-}
-
-//Задание 139.2
-
-let number = 23454;
-let num_str = String(number);
-
-if((num_str[num_str.length-1]%2)==0){
-    console.log('Четное');
+for (let kk in obj){
+	console.log(obj[kk]);
 }
